2569e75b44950c0c31f3e6f50b372568f875a9a5
Kastor <KastorDriver@gmail.com>
2018-07-02 23:53:13 +0300

    tests - inception

---
+++ StatisticsService/src/test/java/one/kastordriver/dailyvision/controller/StatisticsControllerTest.java
@@ -0,0 +1,29 @@
+package one.kastordriver.dailyvision.controller;
+
+import one.kastordriver.dailyvision.controlller.StatisticsController;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+/**
+ * Created by Kastor on 7/2/2018.
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(StatisticsController.class)
+@AutoConfigureRestDocs("target/generated-snippets")
+public class StatisticsControllerTest {
+
+//    protected RestDocumentationResultHandler documentation = document("{class-name}/{method-name}",
+//            preprocessResponse(prettyPrint())).snippets();
+
+    @Autowired
+    protected MockMvc mockMvc;
+}
