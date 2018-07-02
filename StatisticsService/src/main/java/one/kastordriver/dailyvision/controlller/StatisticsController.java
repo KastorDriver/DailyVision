5da694b32ffa0ddc6682cde3801a22c771354a08
Kastor <KastorDriver@gmail.com>
2018-07-02 22:42:37 +0300

    initial commit

---
+++ StatisticsService/src/main/java/one/kastordriver/dailyvision/controlller/StatisticsController.java
@@ -0,0 +1,12 @@
+package one.kastordriver.dailyvision.controlller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Kastor on 7/1/2018.
+ */
+@RestController
+public class StatisticsController {
+
+    
+}
